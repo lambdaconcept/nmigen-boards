ad59c278e4f6dd56a4b89f0007f2f951dda56817
Jean-François Nguyen <jf@lambdaconcept.com>
2020-05-12 15:54:11 +0200

    [WIP] ecpix5: add ddr3, hdmi, sata, ulpi and usb-c.

--- nmigen_boards/ecpix5.py
+++ nmigen_boards/ecpix5.py
@@ -56,15 +56,87 @@ class ECPIX5Platform(LatticeECP5Platform):
             attrs=Attrs(IO_TYPE="LVCMOS33"),
         ),
 
-        # TODO
-        # ddr3
-        # sata
-        # ulpi
+        Resource("ddr3", 0,
+            Subsignal("clk",    DiffPairs("H3", "J3", dir="o"), Attrs(IO_TYPE="SSTL135D_I")),
+            Subsignal("clk_en", Pins("P1", dir="o")),
+            Subsignal("we",     PinsN("R3", dir="o")),
+            Subsignal("ras",    PinsN("T3", dir="o")),
+            Subsignal("cas",    PinsN("P2", dir="o")),
+            Subsignal("a",      Pins("T5 M3 L3 V6 K2 W6 K3 L1 H2 L2 N1 J1 M1 K1", dir="o")),
+            Subsignal("ba",     Pins("U6 N3 N4", dir="o")),
+            Subsignal("dqs",    DiffPairs("V4 V1", "U5 U2", dir="io"), Attrs(IO_TYPE="SSTL135D_I")),
+            Subsignal("dq",     Pins("T4 W4 R4 W5 R6 P6 P5 P4 R1 W3 T2 V3 U3 W1 T1 W2", dir="io")),
+            Subsignal("dm",     Pins("J4 H5", dir="o")),
+            Subsignal("odt",    Pins("L2", dir="o")),
+            Attrs(IO_TYPE="SSTL135_I")
+        ),
+
+        Resource("hdmi", 0,
+            Subsignal("rst",   PinsN("N6", dir="o")),
+            Subsignal("scl",   Pins("C17", dir="io")),
+            Subsignal("sda",   Pins("E17", dir="io")),
+            Subsignal("pclk",  Pins("C1", dir="o")),
+            Subsignal("vsync", Pins("A4", dir="o")),
+            Subsignal("hsync", Pins("B4", dir="o")),
+            Subsignal("de",    Pins("A3", dir="o")),
+            Subsignal("d",
+                Subsignal("b", Pins("AD25 AC26 AB24 AB25  B3  C3  D3  B1  C2  D2 D1 E3", dir="o")),
+                Subsignal("g", Pins("AA23 AA22 AA24 AA25  E1  F2  F1 D17 D16 E16 J6 H6", dir="o")),
+                Subsignal("r", Pins("AD26 AE25 AF25 AE26 E10 D11 D10 C10  D9  E8 H5 J4", dir="o")),
+            ),
+            Subsignal("mclk",  Pins("E19", dir="o")),
+            Subsignal("sck",   Pins("D6", dir="o")),
+            Subsignal("ws",    Pins("C6", dir="o")),
+            Subsignal("i2s",   Pins("A6 B6 A5 C5", dir="o")),
+            Subsignal("int",   PinsN("C4", dir="i")),
+            Attrs(IO_TYPE="LVTTL33")
+        ),
+
+        Resource("sata", 0,
+            Subsignal("tx", DiffPairs("AD16", "AD17", dir="o")),
+            Subsignal("rx", DiffPairs("AF15", "AF16", dir="i")),
+            Attrs(IO_TYPE="LVDS")
+        ),
+
+        Resource("ulpi", 0,
+            Subsignal("rst",  PinsN("E23", dir="o")),
+            Subsignal("clk",  Pins("H24", dir="i")),
+            Subsignal("dir",  Pins("F22", dir="i")),
+            Subsignal("nxt",  Pins("F23", dir="i")),
+            Subsignal("stp",  Pins("H23", dir="o")),
+            Subsignal("data", Pins("M26 L25 L26 K25 K26 J23 J26 H25", dir="io")),
+            Attrs(IO_TYPE="LVCMOS33")
+        ),
+
+        Resource("usbc_cfg", 0,
+            Subsignal("scl", Pins("D24", dir="io")),
+            Subsignal("sda", Pins("C24", dir="io")),
+            Subsignal("dir", Pins("B23", dir="i")),
+            Subsignal("id",  Pins("D23", dir="i")),
+            Subsignal("int", PinsN("B24", dir="i")),
+            Attrs(IO_TYPE="LVCMOS33")
+        ),
+        Resource("usbc_mux", 0,
+            Subsignal("en",    Pins("C23", dir="oe")),
+            Subsignal("amsel", Pins("B26", dir="oe")),
+            Subsignal("pol",   Pins("D26", dir="o")),
+            Subsignal("lna",   DiffPairs( "AF9", "AF10", dir="i"), Attrs(IO_TYPE="LVCMOS18D")),
+            Subsignal("lnb",   DiffPairs("AD10", "AD11", dir="o"), Attrs(IO_TYPE="LVCMOS18D")),
+            Subsignal("lnc",   DiffPairs( "AD7",  "AD8", dir="o"), Attrs(IO_TYPE="LVCMOS18D")),
+            Subsignal("lnd",   DiffPairs( "AF6",  "AF7", dir="i"), Attrs(IO_TYPE="LVCMOS18D")),
+            Attrs(IO_TYPE="LVCMOS33")
+        ),
     ]
 
     connectors  = [
         Connector("pmod", 0, "T25 U25 U24 V24 - - T26 U26 V26 W26 - -"),
+        Connector("pmod", 1, "U23 V23 U22 V21 - - W25 W24 W23 W22 - -"),
+        Connector("pmod", 2, "J24 H22 E21 D18 - - K22 J21 H21 D22 - -"),
+        Connector("pmod", 3, " E4  F4  E6  H4 - -  F3  D4  D5  F5 - -"),
         Connector("pmod", 4, "E26 D25 F26 F25 - - A25 A24 C26 C25 - -"),
+        Connector("pmod", 5, "D19 C21 B21 C22 - - D21 A21 A22 A23 - -"),
+        Connector("pmod", 6, "C16 B17 C18 B19 - - A17 A18 A19 C19 - -"),
+        Connector("pmod", 7, "D14 B14 E14 B16 - - C14 A14 A15 A16 - -"),
     ]
 
     @property
